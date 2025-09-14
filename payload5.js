d9e5e6403f06572ac68f60520f5568d9abcb5c12
yuvaraj <yuvaraj.anbazhagan@syncfusion.com>
2025-09-14 20:51:56 +0530

    second commit

--- payload5.js
+++ payload5.js
@@ -1,11 +1,1 @@
-
-// Payload
-var data = JSON.stringify({
-    makeActive: true,
-    userID: "11977"
-});
-
-// Send request
-xhr.send(data);
-console.log("Done");
-test();
+console.log("Initial commit");
