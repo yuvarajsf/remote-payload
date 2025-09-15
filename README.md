ba1ae5030cf21d268641a448ad0d932dabfb4401
yuvaraj <yuvaraj.anbazhagan@syncfusion.com>
2025-09-16 00:37:48 +0530

    it's again going on

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # remote-payload
 ## Again man Again
 # it's commit with commit file list
+## that was mistake
--- payload5.js
+++ payload5.js
@@ -1,3 +1,4 @@
 console.log("Again man Again");
 confirm.log("Yeah Again and again");
 console.log("finaly with commit files list");
+console.log("Again man");
